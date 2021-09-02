c554429f401fc6b5ed25917041c7d5d6da20827a
danielvu-31 <duchongvu01@gmail.com>
2021-09-02 11:12:25 -0400

    optimizers

---
+++ optimizer/__init__.py
@@ -0,0 +1,5 @@
+from .sam import SAM
+from .pcgrad import PCGrad
+from .utils import load_optimizer
+
+__all__ = ["SAM", "PCGrad", "load_optimizer"]
---
+++ optimizer/utils.py
@@ -0,0 +1,15 @@
+from torch.optim import Adam
+from pcgrad import PCGrad
+from sam import SAM
+
+def load_optimizer(model, lr, optimizer_name, weight_decay):
+    if optimizer_name == "sam":
+        optimizer = SAM(filter(lambda p: p.requires_grad, model.parameters()),
+                        Adam, lr=lr, weight_decay=weight_decay)
+    elif optimizer_name == "pcgrad":
+        optimizer = PCGrad(Adam(model.parameters()))
+    else:
+        optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()),
+                         lr=lr, weight_decay=weight_decay)
+    return optimizer
+
