ea7a28782e03c0e83710000642e577c8fafb8f0a
vudh3 <vudh3@miamioh.edu>
2021-09-05 11:06:12 -0400

    Add requires_grad control

---
+++ weights/__init__.py
@@ -0,0 +1,3 @@
+from .weight_freezer import Freezer
+
+__all__ = ["Freezer"]
--- weights/weight_freezer.py
+++ weights/weight_freezer.py
@@ -1,13 +1,58 @@
 import torch
 import torch.nn as nn
 
-from models.full_model import ContextAwareAttention
-
 
 class Freezer():
-    def __init__(self, model, phase):
+    def __init__(self, model):
+        self._set_parameter_requires_grad([model], False)
         self.model = model
-        self.phase = phase
     
-    def freeze_weight(self):
-        pass
+    def freeze_weight(self, phase):
+        if phase <= 1:
+            self._set_parameter_requires_grad([self.model.context_module, self.model.gating_module], False)
+            self._set_parameter_requires_grad([self.model.face_module.classifier_age,
+                                                self.model.face_module.classifier_emotion,
+                                                self.model.face_module.encoder_att_1,
+                                                self.model.face_module.encoder_att_2,
+                                                self.model.face_module.encoder_att_3,
+                                                self.model.face_module.encoder_att_4,
+                                                self.model.face_module.encoder_block_att_1,
+                                                self.model.face_module.encoder_block_att_2,
+                                                self.model.face_module.encoder_block_att_3], 
+                                                True)
+            self.model.forward_phase = "face"
+        elif phase <= 2:
+            self._set_parameter_requires_grad([self.model.context_module, self.model.gating_module], False)
+            self._set_parameter_requires_grad([self.model.face_module], True)
+            self.model.forward_phase = "face"
+        elif phase <= 3:
+            self._set_parameter_requires_grad([self.model.face_module, self.model.gating_module], False)
+            self._set_parameter_requires_grad([self.model.context_module.classifier_age,
+                                                self.model.context_module.classifier_emotion,
+                                                self.model.context_module.encoder_att_1,
+                                                self.model.context_module.encoder_att_2,
+                                                self.model.context_module.encoder_att_3,
+                                                self.model.context_module.encoder_att_4,
+                                                self.model.context_module.encoder_block_att_1,
+                                                self.model.context_module.encoder_block_att_2,
+                                                self.model.context_module.encoder_block_att_3],
+                                                True)
+            self.model.forward_phase = "context"
+        elif phase <= 4:
+            self._set_parameter_requires_grad([self.model.face_module, self.model.gating_module], False)
+            self._set_parameter_requires_grad([self.model.context_module], True)
+        elif phase <= 5:
+            self._set_parameter_requires_grad([self.model.face_module, self.model.context_module], False)
+            self._set_parameter_requires_grad([self.model.gating_module], True)
+        else:
+            self._set_parameter_requires_grad([self.model], True)
+
+    def _return_model(self):
+        return self.model
+
+    @staticmethod
+    def _set_parameter_requires_grad(model_list, requires_grad):
+        for model in model_list:
+            for param in model.parameters():
+                param.requires_grad = requires_grad
+
